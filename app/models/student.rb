f014f6b91e611ecbcf8fda647a27c2ab0d3fe479
Thiago Freire <freireag@gmail.com>
2010-07-08 10:50:30 -0300

    Adding marital status select

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ActiveRecord::Base
+  MARITAL_STATUS = {:casado => "Casado(a)", :divorciado => "Divorciado(a)", :solteiro => "Solteiro(a)", :viuvo => "Viúvo(a)"}
   # Include default devise modules. Others available are:
   # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable, :registerable, :validatable and :activatable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :authentication_keys => [:cpf]
